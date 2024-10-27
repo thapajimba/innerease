0f2b48aacf4dd65c2e3f4bb191adb1243be5bdf9
unknown <thapajimba@gmail.com>
2024-10-27 13:40:20 -0700

    Replaced a few thing.

--- app/(dashboard)/(landingpage)/page.tsx
+++ app/(dashboard)/(landingpage)/page.tsx
@@ -39,7 +39,7 @@ export default function HomePage() {
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                  Welcome to MindfulCare
+                  Welcome to Inner Ease
                 </h1>
                 <p className="mx-auto max-w-[700px] text-gray-600 md:text-xl">
                   Empowering you with daily wellness tips, inspiration, and personalized guidance. Pay per service and show
